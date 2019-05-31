9d23e1b4b028ace31d1dafa2247e7759d833096b
satishpabbineedi <50616204+satishpabbineedi@users.noreply.github.com>
2019-05-31 14:28:29 +0530

    Add files via upload

--- green house monitoring.ino
+++ green house monitoring.ino
@@ -2,7 +2,9 @@
 // Written by ladyada, public domain
 
 #include "DHT.h"
-
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+SoftwareSerial s(5,6);
 #define DHTPIN 2     // what pin we're connected to
 
 // Uncomment whatever type you're using!
@@ -22,25 +24,38 @@
 // tweak the timings for faster processors.  This parameter is no longer needed
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
+float h,t,f,hic,hif;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("DHTxx test!");
-
+  s.begin(9600);
   dht.begin();
 }
 
 void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
+  int smoke=analogRead(A0);
+  int gas=analogRead(A1);
+  int ldr=analogRead(A2);
+  int mosi=analogRead(A3);
+  Serial.print("smoke=");
+  Serial.println(smoke);
+  Serial.print("gas=");
+  Serial.println(gas);
+  Serial.print("ldr=");
+  Serial.println(ldr);
+Serial.print("mosi=");
+  Serial.println(mosi);
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+  h = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+  t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
@@ -49,21 +64,59 @@ void loop() {
   }
 
   // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
+  hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  hic = dht.computeHeatIndex(t, h, false);
 
   Serial.print("Humidity: ");
   Serial.print(h);
-  Serial.print(" %\t");
+  Serial.print(" %\n");
   Serial.print("Temperature: ");
   Serial.print(t);
   Serial.print(" *C ");
   Serial.print(f);
-  Serial.print(" *F\t");
+  Serial.print(" *F\n");
   Serial.print("Heat index: ");
   Serial.print(hic);
   Serial.print(" *C ");
   Serial.print(hif);
   Serial.println(" *F");
+  delay(3000);
+  sendAndroidValues();
+/*  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["data1"] = smoke;
+  root["data2"] = gas;
+  root["data3"] = mosi;
+  root["data4"] = ldr;
+  root["data5"] = t;
+  root["data6"] = h;
+if(s.available()>0)
+{
+ root.printTo(s);
+} */
+}
+void sendAndroidValues()
+{
+  //puts # before the values so our app knows what to do with the data
+  s.print('#');
+  Serial.print('#');
+  //for loop cycles through 2 sensors and sends values via serial
+    String payload = "{\"d\":{\"humidity\":";
+  payload += h;
+  payload+="," "\"tempc\":";
+  payload += t;
+  payload+="," "\"tempf\":";
+  payload += f;
+  payload+="," "\"hic\":";
+  payload += hic;
+  payload+="," "\"hif\":";
+  payload += hif;
+  payload += "}}";
+ Serial.print("Sending payload: ");
+ Serial.println(payload);
+ s.print(payload);
+  s.println('~'); //used as an end of transmission character - used in app for string length
+  Serial.println('~');
+  delay(3000);        //added a delay to eliminate missed transmissions
 }
